2ca29b11bb4ebfdb535154e4ad3443eaabcbc95f
Marcus Bernroth <marcusbernroth@gmail.com>
2026-04-01 16:58:20 +0200

    Add get and post handlers

--- src/app/api/books/route.ts
+++ src/app/api/books/route.ts
@@ -1,1 +1,34 @@
-// get and post
+import { NextResponse, type NextRequest } from "next/server";
+import { getCloudflareContext } from "@opennextjs/cloudflare";
+import { type Book } from "@/app/types";
+
+export async function GET() {
+  const { env } = await getCloudflareContext({ async: true });
+  const db = env.books;
+
+  const result = await db.prepare("SELECT * FROM books").all<Book[]>();
+
+  return NextResponse.json(result);
+}
+
+export async function POST(request: NextRequest) {
+  const { env } = await getCloudflareContext({ async: true });
+  const db = env.books;
+
+  const { title, author, publish_date, genre } = (await request.json()) as Omit<
+    Book,
+    "id"
+  >;
+
+  const result = await db
+    .prepare(
+      "INSERT INTO books (title, author, publish_date, genre) VALUES (?, ?, ?, ?)",
+    )
+    .bind(title, author, publish_date, genre)
+    .run();
+
+  return NextResponse.json(
+    { id: result.meta.last_row_id, title, author, publish_date, genre },
+    { status: 201 },
+  );
+}
